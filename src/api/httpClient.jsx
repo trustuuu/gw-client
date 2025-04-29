cab409670565f743fa16dd617ef0cd21a2da98d5
James YS Chang <trustuu@gmail.com>
2025-04-29 11:47:43 -0700

    small change

--- src/api/httpClient.jsx
+++ src/api/httpClient.jsx
@@ -1,9 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
 
 const httpClient = axios.create();
 
 const setHttpClient = (header) => {
-    httpClient.defaults.headers = header;
-}
+  httpClient.defaults.headers = header;
+};
 
-export {httpClient, setHttpClient};
+// httpClient.interceptors.request.use((config) => {
+// const token = localStorage.getItem('authToken'); // or sessionStorage, or Recoil, etc.
+// if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+// }
+// return config;
+// });
+
+// httpClient.interceptors.response.use(
+//     response => response,
+//     async error => {
+//       if (error.response?.status === 401) {
+//         // try to refresh token logic here
+//       }
+//       return Promise.reject(error);
+//     }
+// );
+
+export { httpClient, setHttpClient };
